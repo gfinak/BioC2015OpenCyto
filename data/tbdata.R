f08817d8cf6953a78763b46c26e093d37917a44f
Greg Finak <greg.finak@gmail.com>
2015-07-14 17:39:22 -0700

    allow nc file to be downloaded from dropbox

--- data/tbdata.R
+++ data/tbdata.R
@@ -1,7 +1,16 @@
 library(flowWorkspace)
 f = system.file(package="BioC2015OpenCyto","extdata")
 if(f!=""){
-  tbdata = load_gs(file.path(f,"tbdata"))
+  if(file.exists(file.path(f,"tbdata","file1320c19b087dc.nc"))){
+    tbdata = load_gs(file.path(f,"tbdata"))
+  }else{
+    message("Downloading an 800 Mb file. Please be patient. If this fails, you may need to install wget.")
+    download.file(
+      url = "http://dl.dropboxusercontent.com/u/2635312/file1320c19b087dc.nc",method =
+        "wget",mode = "wb",destfile = file.path(f,"tbdata",
+                                                "file1320c19b087dc.nc"), quiet = TRUE
+    )
+  }
 }else{
   message("Not loading data until package is installed")
 }
